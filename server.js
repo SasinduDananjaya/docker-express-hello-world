7192ec54d114ad403ce748a9c6e8488233320daa
Sasindu Gunawardana <sasindudananjaya2000@gmail.com>
2024-07-07 23:21:08 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5001
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+})
